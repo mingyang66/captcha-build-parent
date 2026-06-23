d4d637a0c751468702c3d1169f5e19b05d30feff
yaomingyang <mingyang@eastmoney.com>
2026-06-23 14:45:40 +0800

    feature:update code

--- captcha-spring-boot-sample/src/main/java/com/emily/captcha/controller/CaptchaController.java
+++ captcha-spring-boot-sample/src/main/java/com/emily/captcha/controller/CaptchaController.java
@@ -7,7 +7,10 @@ import com.emily.captcha.rotate.model.RotateCaptcha;
 import com.emily.captcha.rotate.service.RotateCaptchaService;
 import com.emily.captcha.slider.model.SliderCaptcha;
 import com.emily.captcha.slider.service.SliderCaptchaService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
