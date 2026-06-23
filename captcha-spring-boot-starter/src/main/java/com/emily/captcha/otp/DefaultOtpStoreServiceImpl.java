d6e5e26656ca9287c1e8a0cc760fbb25af3f3ca4
yaomingyang <mingyang@eastmoney.com>
2026-06-23 15:32:40 +0800

    feature:新增OTP双因子认证功能

---
+++ captcha-spring-boot-starter/src/main/java/com/emily/captcha/otp/DefaultOtpStoreServiceImpl.java
@@ -0,0 +1,31 @@
+package com.emily.captcha.otp;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 基于内存的OTP会话存储默认实现
+ */
+public class DefaultOtpStoreServiceImpl implements OtpStoreService {
+
+    private final ConcurrentHashMap<String, OtpSession> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void put(String account, OtpSession session) {
+        store.put(account, session);
+    }
+
+    @Override
+    public OtpSession get(String account) {
+        return store.get(account);
+    }
+
+    @Override
+    public void update(String account, OtpSession session) {
+        store.put(account, session);
+    }
+
+    @Override
+    public void remove(String account) {
+        store.remove(account);
+    }
+}
---
+++ captcha-spring-boot-starter/src/main/java/com/emily/captcha/otp/OtpStoreService.java
@@ -0,0 +1,38 @@
+package com.emily.captcha.otp;
+
+/**
+ * OTP会话存储服务接口
+ */
+public interface OtpStoreService {
+
+    /**
+     * 存储OTP会话
+     *
+     * @param account 账户标识
+     * @param session OTP会话数据
+     */
+    void put(String account, OtpSession session);
+
+    /**
+     * 获取OTP会话
+     *
+     * @param account 账户标识
+     * @return OTP会话数据，如果不存在则返回null
+     */
+    OtpSession get(String account);
+
+    /**
+     * 更新OTP会话
+     *
+     * @param account 账户标识
+     * @param session OTP会话数据
+     */
+    void update(String account, OtpSession session);
+
+    /**
+     * 删除OTP会话
+     *
+     * @param account 账户标识
+     */
+    void remove(String account);
+}
